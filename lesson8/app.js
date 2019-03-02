1026091fcca131fafad1a4a39545a0bfd9667dff
a04watson <a04watson@gmail.com>
2019-03-02 16:01:35 +0800

    .lesson8

--- lesson8/app.js
+++ lesson8/app.js
@@ -131,21 +131,21 @@ function update(){
 	game.physics.arcade.collide(stars,platforms);
 	game.physics.arcade.collide(baddie,platforms);
 
-	// //player not doing anything or default condition
-	// player.body.velocity.x = 0;
+	//player not doing anything or default condition
+	player.body.velocity.x = 0;
 
-	// //do the player control
-	// if(cursors.left.isDown) {
-	// 	player.body.velocity.x = -150;
-	// 	player.animations.play('left');
-	// } else if (cursors.right.isDown) {
-	// 	player.body.velocity.x = 150;
-	// 	player.animations.play('right');
-	// } else {
-	// 	player.animations.stop();
-	// 	player.frame = 4;
-	// }
-	// if (cursors.up.isDown && player.body.touching.down) {
-	// 	player.body.velocity.y = -300;
-	// }
+	//do the player control
+	if(cursors.left.isDown) {
+		player.body.velocity.x = -150;
+		player.animations.play('left');
+	} else if (cursors.right.isDown) {
+		player.body.velocity.x = 150;
+		player.animations.play('right');
+	} else {
+		player.animations.stop();
+		player.frame = 4;
+	}
+	if (cursors.up.isDown && player.body.touching.down) {
+		player.body.velocity.y = -300;
+	}
 }
